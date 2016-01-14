19555d8153b49dd3cf102e822a5dd73d7696b2ae
heyanfang <15010368305@163.com>
2016-01-14 18:16:47 +0800

    1/14

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
 var toutiao_url = "http://games.hoolai.com/cms/?post_id=6643&json=get_post&include=title";
+var news_url="http://games.hoolai.com/cms/?cat=227&json=get_category_posts&include=title,categories,date&count=500";
+var Url="http://games.hoolai.com/cms/?json=get_category_posts&cat=208&include=title,content,custom_fields,date,categories&count=500";
 
 $(document).ready(function(){
-   
-
-    request(toutiao_url, function(err, data) { //今日头条
+    request(toutiao_url, function(err, data) {
         if (err) {
             console.log(err);
         } else {
             var _data = data.post;
-            // console.log(data.custom_fields);
-            // console.log(_data.length);
             if (_data) {
                 $('div.news_bg a.toutiao').text(_data.title).attr('href', 'newsContent.html#post_id=' + _data.id);
             } else {
@@ -19,28 +17,73 @@ $(document).ready(function(){
 
         }
     });
+
+    request(Url,function(err,data){
+        if(err){console.log(err);}
+        else{
+            /*var Newdata=data.post;*/
+            var posts='';
+            var total=data.posts.length;
+            var rows=6;
+            var p=-1;
+            for(var i=0;i<total;i++)
+            {
+                var post=data.posts[i];
+                if(i%rows==0){//6/16=0  11/5=
+                    p++;
+                }
+        
+            if(post.custom_fields.date){
+            posts+='<li class="page'+p+'"><a href="newsContent.html?&post_id='+post.id+'">'+post.title+'</a><i>'+post.custom_fields.date+'</i></li>';
+            }
+            else{
+                posts+='<li class="page'+p+'"><a href="newsContent.html?post_id='+post.id+'">'+post.title+'</a></li>';
+            }
+        }
+        index.countPage=p;
+     /*   debugger*/
+        index.page=0;
+        document.getElementById("pages").innerHTML=posts;
+        $('#pages').find('li').hide();
+        $('.page0').show(); 
+           }
+    })
 })
+/*
+var index={};
+function clickNextPage(){
 
+    index.page++;
+    $(".page"+index.page).show();
+    if(index.page==index.countPage){
+        $(".more").hide();
+    }
+}*/
+var index={};
+function clickNextPage(){
 
-
-
-
+    index.page++;
+    $(".page"+index.page).show();
+    if(index.page==index.countPage){
+        $(".more").hide();
+    }
+}
 
 function getDataList(cat, posts) {
+
     var posts_len = posts.length;
     var dataList = [];
     if (posts_len == 0) {
         return null;
     }
-    for (var j = 0; j < posts_len; j++) {
-        var cat_len = posts[j].categories.length;
-
-        for (var i = cat_len - 1; i >= 0; i--) {
-            if (posts[j].categories[i].id == cat) {
-                dataList.push(posts[j]);
-            }
-        }
-
+    for(var i=0;i<posts_len;i++)
+    {
+    var cat_leng=posts[i].categories.length;
+     for(var j=0;j<cat_leng;j++)
+     {
+      if(posts[i].categories[j].id==cat)
+        dataList.push(posts[i]);
+     }
     }
     return dataList;
 }
